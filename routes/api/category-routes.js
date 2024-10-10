7b492df70d3bb93eedbbb2397d1ac6b8dcfdc561
sgforcert <sgforcert@gmail.com>
2024-10-09 21:50:56 -0700

    ecommerce

---
+++ routes/api/category-routes.js
@@ -0,0 +1,94 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+    include: [{ model: Product }]
+  });
+  if (!categoryData) {
+    res.status(404).json({ message: 'No categories found'});
+    return;
+  }
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product }]
+  });
+  if (!categoryData) {
+    res.status(404).json({ message: 'No categories found with this id'});
+    return;
+  }
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  // create a new category
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      /*{
+        category_name: req.body.category_name
+      },*/
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    if (!categoryData) {
+      return res.status(404).json({ message: 'No category found with this id!' });
+    }
+    console.log("Updating: ", categoryData);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // update a category by its `id` value
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // delete a category by its `id` value
+});
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,32 @@
+const seedCategories = require('./category-seeds');
+const seedProducts = require('./product-seeds');
+const seedTags = require('./tag-seeds');
+const seedProductTags = require('./product-tag-seeds');
+
+const sequelize = require('../config/connection');
+const { Category, ProductTag, Product, Tag } = require('../models');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  // await Category.sync();
+  // await Product.sync();
+  // await Tag.sync();
+  // await ProductTag.sync();
+
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+
+  await seedTags();
+  console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedProductTags();
+  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
